eff75e2c3befa82bc8e2c94afb2882b04470952b
Martin Janik <martin.janik@medfusion.com>
2017-09-26 12:54:43 +0200

    dummy tick working

--- src/components/basic_list_item.js
+++ src/components/basic_list_item.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const BasicListItem = (props) => {
-    return (
-        <li className="list-group-item">
-            <div>
-                <div className="btn-group inlineButtons">
-                    <button className="btn btn-default tickButton" onClick=''>
-                        <span className="glyphicon glyphicon-ok" aria-hidden="true" />
-                    </button>
-                    <button className="btn btn-default deleteButton" onClick={props.deleteItem}>
-                        <span className="glyphicon glyphicon-remove" aria-hidden="true" />
-                    </button>
+class BasicListItem extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+           tickUntickItem: 'unticked'
+        }
+    }
+
+    toggleTickItem() {
+        const css = (this.state.tickUntickItem === 'unticked') ? 'ticked' : 'unticked';
+        this.setState({tickUntickItem: css});
+    }
+
+    render() {
+        return (
+            <li className={"list-group-item " + this.state.tickUntickItem}>
+                <div className="">
+                    <div className="btn-group inlineButtons">
+                        <button className="btn btn-default tickButton" onClick={this.toggleTickItem.bind(this)}>
+                            <span className="glyphicon glyphicon-ok" aria-hidden="true" />
+                        </button>
+                        <button className="btn btn-default deleteButton" onClick={this.props.deleteItem}>
+                            <span className="glyphicon glyphicon-remove" aria-hidden="true" />
+                        </button>
+                    </div>
+                        {this.props.item}
                 </div>
-                {props.item}
-            </div>
-        </li>
-    );
+            </li>
+        );
+    }
 }
 
 export default BasicListItem;
